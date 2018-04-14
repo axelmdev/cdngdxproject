8e74d270f911ad6dd2b6a68f9767857353d0aacc
Axel Mainguy <killerbossx@gmail.com>
2018-04-15 01:50:56 +0200

    Add Demo Button

--- desktop/src/fr/imie/ena/fightforgrades/desktop/DesktopLauncher.java
+++ desktop/src/fr/imie/ena/fightforgrades/desktop/DesktopLauncher.java
@@ -3,10 +3,11 @@ package fr.imie.ena.fightforgrades.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import fr.imie.ena.fightforgrades.FightForGrades;
+import fr.imie.ena.fightforgrades.UIDemo;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new FightForGrades(), config);
+		new LwjglApplication(new UIDemo(), config);
 	}
 }
