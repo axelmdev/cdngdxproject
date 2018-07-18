4afc23ae5148ff66ed7d0d2e2f7075d45aa6d59d
Axel Mainguy <killerbossx@gmail.com>
2018-07-18 11:11:37 +0200

    Add Screens & Update MainMenu & GameWin Screen

--- core/src/fr/imie/ena/fightforgrades/screens/LoginScreen.java
+++ core/src/fr/imie/ena/fightforgrades/screens/LoginScreen.java
@@ -1,8 +1,110 @@
 package fr.imie.ena.fightforgrades.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.utils.Align;
+
+import fr.imie.ena.fightforgrades.FightForGrades;
+import fr.imie.ena.fightforgrades.GameConstants;
+
 /**
  * Created by Axel on 17/06/2018.
  */
 
-public class LoginScreen {
+public class LoginScreen implements Screen {
+    final FightForGrades game;
+    /*private Texture badlogic;*/
+    private Skin mySkin;
+    private Stage stage;
+
+    public LoginScreen(final FightForGrades game) {
+        this.game = game;
+        game.myAssetManager.queueAddSkin();
+        game.myAssetManager.manager.finishLoading();
+        mySkin = game.myAssetManager.manager.get(GameConstants.skin);
+        stage = new Stage(game.screenPort);
+        Gdx.input.setInputProcessor(stage);
+
+        Label gameTitle = new Label("FIGHT FOR GRADES",mySkin,"big");
+        gameTitle.setSize(GameConstants.col_width*2,GameConstants.row_height*2);
+        gameTitle.setPosition(GameConstants.centerX - gameTitle.getWidth()/2,GameConstants.centerY + GameConstants.row_height);
+        gameTitle.setAlignment(Align.center);
+
+        final TextField usernameTextField = new TextField("", mySkin, "default");
+        usernameTextField.setPosition(GameConstants.centerX - usernameTextField.getWidth()/2,GameConstants.centerY);
+        usernameTextField.setSize(GameConstants.col_width*2,GameConstants.row_height);
+
+        final TextField passwordField = new TextField("", mySkin, "default");
+        passwordField.setPosition(GameConstants.centerX - passwordField.getWidth()/2,usernameTextField.getY() - GameConstants.row_height -15);
+        passwordField.setSize(GameConstants.col_width*2,GameConstants.row_height);
+
+        Button startBtn = new TextButton("Connexion",mySkin,"small");
+        startBtn.setSize(GameConstants.col_width*2,GameConstants.row_height);
+        startBtn.setPosition(GameConstants.centerX - startBtn.getWidth()/2,passwordField.getY() - GameConstants.row_height -15);
+        startBtn.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new LoadingScreen(game, usernameTextField, passwordField));
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
+
+        stage.addActor(usernameTextField);
+        stage.addActor(passwordField);
+        stage.addActor(startBtn);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        game.screenPort.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        mySkin.dispose();
+        stage.dispose();
+
+    }
 }
--- core/src/fr/imie/ena/fightforgrades/screens/SelectPartyScreen.java
+++ core/src/fr/imie/ena/fightforgrades/screens/SelectPartyScreen.java
@@ -11,36 +11,38 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.Align;
-import fr.imie.ena.fightforgrades.GameConstants;
-import fr.imie.ena.fightforgrades.FightForGrades;
 
-public class MainMenuScreen implements Screen {
+import fr.imie.ena.fightforgrades.FightForGrades;
+import fr.imie.ena.fightforgrades.GameConstants;
+
+public class SelectPartyScreen implements Screen {
     final FightForGrades game;
     /*private Texture badlogic;*/
     private Skin mySkin;
     private Stage stage;
 
-    public MainMenuScreen(final FightForGrades game){
+    public SelectPartyScreen(final FightForGrades game) {
         this.game = game;
         game.myAssetManager.queueAddSkin();
         game.myAssetManager.manager.finishLoading();
         mySkin = game.myAssetManager.manager.get(GameConstants.skin);
+        /*badlogic = new Texture(Gdx.files.internal("badlogic.jpg"));*/
+        /*mySkin = new Skin(Gdx.files.internal(GameConstants.skin));*/
         stage = new Stage(game.screenPort);
         Gdx.input.setInputProcessor(stage);
 
-
-        Label gameTitle = new Label("FIGHT FOR GRADES",mySkin,"big");
+        Label gameTitle = new Label("Select a game",mySkin,"big");
         gameTitle.setSize(GameConstants.col_width*2,GameConstants.row_height*2);
         gameTitle.setPosition(GameConstants.centerX - gameTitle.getWidth()/2,GameConstants.centerY + GameConstants.row_height);
         gameTitle.setAlignment(Align.center);
 
-        Button startBtn = new TextButton("Start Game",mySkin,"small");
-        startBtn.setSize(GameConstants.col_width*2,GameConstants.row_height);
-        startBtn.setPosition(GameConstants.centerX - startBtn.getWidth()/2,GameConstants.centerY);
-        startBtn.addListener(new InputListener(){
+        Button party1Btn = new TextButton("Party 1",mySkin,"small");
+        party1Btn.setSize(GameConstants.col_width*2,GameConstants.row_height);
+        party1Btn.setPosition(GameConstants.centerX - party1Btn.getWidth()/2,GameConstants.centerY);
+        party1Btn.addListener(new InputListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                game.setScreen(new SelectPartyScreen((FightForGrades) game));
+                game.setScreen(new PlayScreen((FightForGrades) game));
                 return true;
             }
 
@@ -50,13 +52,13 @@ public class MainMenuScreen implements Screen {
             }
         });
 
-        Button settingsBtn = new TextButton("Settings",mySkin,"small");
-        settingsBtn.setSize(GameConstants.col_width*2,GameConstants.row_height);
-        settingsBtn.setPosition(GameConstants.centerX - settingsBtn.getWidth()/2,startBtn.getY() - GameConstants.row_height -15);
-        settingsBtn.addListener(new InputListener(){
+        Button party2Btn = new TextButton("Party 2",mySkin,"small");
+        party2Btn.setSize(GameConstants.col_width*2,GameConstants.row_height);
+        party2Btn.setPosition(GameConstants.centerX - party2Btn.getWidth()/2,party1Btn.getY() - GameConstants.row_height -15);
+        party2Btn.addListener(new InputListener(){
             @Override
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                game.setScreen(new SettingsScreen((FightForGrades) game));
+                game.setScreen(new PlayScreen((FightForGrades) game));
                 return true;
             }
 
@@ -66,13 +68,28 @@ public class MainMenuScreen implements Screen {
             }
         });
 
+        Button party3Btn = new TextButton("Party 3",mySkin,"small");
+        party3Btn.setSize(GameConstants.col_width*2,GameConstants.row_height);
+        party3Btn.setPosition(GameConstants.centerX - party3Btn.getWidth()/2,party2Btn.getY() - GameConstants.row_height -15);
+        party3Btn.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new PlayScreen((FightForGrades) game));
+                return true;
+            }
+
+            @Override
+            public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+                super.touchUp(event, x, y, pointer, button);
+            }
+        });
 
         stage.addActor(gameTitle);
-        stage.addActor(startBtn);
-        stage.addActor(settingsBtn);
-
-
+        stage.addActor(party1Btn);
+        stage.addActor(party2Btn);
+        stage.addActor(party3Btn);
     }
+
 
     @Override
     public void show() {
@@ -85,8 +102,6 @@ public class MainMenuScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
-
-
     }
 
     @Override
@@ -98,8 +113,6 @@ public class MainMenuScreen implements Screen {
     public void pause() {
 
     }
-
-
 
     @Override
     public void resume() {
@@ -115,6 +128,5 @@ public class MainMenuScreen implements Screen {
     public void dispose() {
         mySkin.dispose();
         stage.dispose();
-
     }
 }
